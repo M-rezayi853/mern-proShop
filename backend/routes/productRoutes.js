49e46e5eed54ba8a780b8a8e5587a0fb885308e6
mahdi <mahdi.rezayi.853@gmail.com>
2021-02-10 19:14:38 +0330

    backend user Authentication

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,31 +1,29 @@
 import express from 'express'
-import asyncHandler from 'express-async-handler'
-import Product from '../models/productModel.js'
+// import asyncHandler from 'express-async-handler'
+// import Product from '../models/productModel.js'
+
+import { getProducts, getProductById } from '../controllers/prdouctController.js'
 
 const router = express.Router()
 
-// @desc    Fetch all products
-// @route    GET /api/products
-// @access    Public
-router.get('/', asyncHandler(async (req, res) => {
-    const products = await Product.find({})
+// router.get('/', asyncHandler(async (req, res) => {
+//     const products = await Product.find({})
     
-    res.json(products)
-}))
+//     res.json(products)
+// }))
+router.route('/').get(getProducts)
 
-// @desc    Fetch single product
-// @route    GET /api/products:/:pid
-// @access    Public
-router.get('/:pid', asyncHandler(async (req, res) => {
-    // const product = products.find(p => p._id === req.params.pid)
-    const product = await Product.findById(req.params.pid)
+// router.get('/:pid', asyncHandler(async (req, res) => {
+//     // const product = products.find(p => p._id === req.params.pid)
+//     const product = await Product.findById(req.params.pid)
 
-    if(product) {
-        res.json(product)
-    } else {
-        res.status(404)
-        throw new Error('Product not found')
-    }
-}))
+//     if(product) {
+//         res.json(product)
+//     } else {
+//         res.status(404)
+//         throw new Error('Product not found')
+//     }
+// }))
+router.route('/:pid').get(getProductById)
 
 export default router
