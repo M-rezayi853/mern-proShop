57ba4d7701d367e4f6854cd79b84a749b7612cf1
mahdi <mahdi.rezayi.853@gmail.com>
2021-02-19 17:22:48 +0330

    front end user authentication and profile

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -50,12 +50,20 @@ const CartScreen = ({ match, location, history }) => {
                                         ${item.price}
                                     </Col>
                                     <Col md={2}>
-                                        <Form.Control as='select' value={item.qty} onChange={(e) => dispatch(addToCart(item.product, Number(e.target.value)))}>
-                                            {[...Array(item.countInStock).keys()].map(x => (
-                                                <option key={x + 1} value={x + 1}>
-                                                    {x + 1}
-                                                </option>
-                                            ))}
+                                        <Form.Control
+                                        as='select'
+                                        value={item.qty}
+                                        onChange={(e) =>
+                                            dispatch(
+                                            addToCart(item.product, Number(e.target.value))
+                                            )
+                                        }
+                                        >
+                                        {[...Array(item.countInStock).keys()].map((x) => (
+                                            <option key={x + 1} value={x + 1}>
+                                            {x + 1}
+                                            </option>
+                                        ))}
                                         </Form.Control>
                                     </Col>
                                     <Col md={2}>
