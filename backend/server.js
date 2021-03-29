ac2691e5dad5aabb0b32a3b28cb3dc114fdada02
mahdi <mahdi.rezayi.853@gmail.com>
2021-03-29 13:50:47 +0430

    Prepare for deployment

--- backend/server.js
+++ backend/server.js
@@ -27,10 +27,6 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('API is running...')
-})
-
 // app.get('/api/products', (req, res) => {
 //     res.json(products)
 // })
@@ -49,6 +45,16 @@ app.get('/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_I
 
 const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+if(process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('API is running...')
+    })
+}
 
 app.use(notFound)
 app.use(errorHandler)
